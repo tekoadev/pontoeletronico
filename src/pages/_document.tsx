1ec86aa5c262302b213f4c6441993e5eb20b2c5b
Fábio Casanova <fabio50019@gmail.com>
2024-07-09 05:53:09 -0300

    feat: adicionado analytics e speed insights

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Document() {
   return (
@@ -7,6 +9,8 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </Html>
   )
